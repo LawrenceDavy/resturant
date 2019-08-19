f74a3e722b184bb9b48b855715b0fb6043a475a6
LawrenceDavy13 <lawrencedavy13@gmail.com>
2019-08-19 20:54:56 +0100

    added slugfield

--- meals/models.py
+++ meals/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Meals(models.Model):
     name = models.CharField(max_length=50)
@@ -7,6 +8,12 @@ class Meals(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     preperation_time = models.IntegerField()
     image = models.ImageField(upload_to='meals/')
+    slug = models.SlugField(blank=True, Null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug and self.name:
+            self.slug = slugify(self.name)
+            super(Meals, self).save(*args, **kwargs)
 
 # name of meal appears admin
 def __str__(self):
