c6fe57dd3c2b36dd52233b5dcd9dde1cc51fe891
LawrenceDavy13 <lawrencedavy13@gmail.com>
2019-08-19 21:04:15 +0100

    Fixed meals subscategory spelling error

--- meals/models.py
+++ meals/models.py
@@ -8,13 +8,19 @@ class Meals(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     preperation_time = models.IntegerField()
     image = models.ImageField(upload_to='meals/')
-    slug = models.SlugField(blank=True, Null=True)
+    slug = models.SlugField(blank=True, null=True)
 
     def save(self, *args, **kwargs):
         if not self.slug and self.name:
             self.slug = slugify(self.name)
             super(Meals, self).save(*args, **kwargs)
 
-# name of meal appears admin
-def __str__(self):
-    return self.name
+    # Fixes meals subscategory spelling error
+    class Meta:
+        verbose_name = 'meal'
+        verbose_name_plural = 'meals'
+
+    # name of meal appears admin
+    def __str__(self):
+        return self.name
+
