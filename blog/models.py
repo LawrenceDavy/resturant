bcfe7f0924320df31830cc008990ddf03b482f11
LawrenceDavy13 <lawrencedavy13@gmail.com>
2019-08-23 22:33:51 +0100

    posts can be added thorugh admin

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Structure for blog
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField() 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # tags 
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+# Groups blog posts into different types
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+
+    # Fixes blog subscategory spelling error
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Category'
+
+    def __str__(self):
+        return self.category_name
+   
