47085c0c61c5d3b93cea0b273dd64968563acd91
LawrenceDavy13 <lawrencedavy13@gmail.com>
2019-08-23 17:36:07 +0100

    meals and cateogry connection made

--- meals/admin.py
+++ meals/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Meals
+from .models import Meals, Category
 
 # Register your models here.
 
 
 # Add meals model to admin site
 admin.site.register(Meals)
+
+# Add category model to admin site 
+admin.site.register(Category)
--- meals/models.py
+++ meals/models.py
@@ -6,6 +6,7 @@ from django.utils.text import slugify
 # Structure for meal information
 class Meals(models.Model):
     name = models.CharField(max_length=50)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True) # Link meals to category. SET_NULL stops category from getting deleted when deleting a meal
     description = models.TextField(max_length=500)
     people = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
@@ -23,7 +24,7 @@ class Meals(models.Model):
         verbose_name = 'meal'
         verbose_name_plural = 'meals'
 
-    # name of meal appears admin
+    # name of meal appears in admin
     def __str__(self):
         return self.name
 
@@ -34,6 +35,12 @@ class Meals(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=30)
 
+    # # Fixes Category subscategory spelling error
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    # Name of category appears in admin
     def __str__(self):
         return self.name
 
