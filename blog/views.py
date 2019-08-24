d293f0cf3118f9692bb8a5914b0ae0247a706661
LawrenceDavy13 <lawrencedavy13@gmail.com>
2019-08-24 08:10:27 +0100

    blog views added

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from .models import Post, Category
 
 def post_list(request):
-    pass
+    post_list = Post.objects.all()
+
+    context = {
+        'post_list': post_list,
+    }
+
+    return render(request, 'Post/post_list.html', context)
+
 
 def post_detail(request, id):
-    pass
+    post_detail = Post.objects.get(id=id)
+
+    context = {
+        'post_detail' : post_detail,
+    }
+
+    return render(request, 'Post/post_detail.html', context)
